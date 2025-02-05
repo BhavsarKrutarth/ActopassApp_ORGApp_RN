a2a37fe6ba93de5f0a636c4e0c289e99775ad9e4
NencySavaliya03 <166900744+NencySavaliya03@users.noreply.github.com>
2025-02-05 11:58:35 +0530

    chnages

--- source/theme/Color.js
+++ source/theme/Color.js
@@ -1,32 +1,32 @@
 const Colors = {
-    Black: '#000000',
-    White: '#ffffff',
-    Text : '#B3B3B3',
-    Placeholder: '#5A5A5A', 
-    line:"#cccccc",
-    purple : '#A115FF',
-    lightgrey : '#EEEEEE',
-    bordercolor:'#E2E2E2',
-    lable :'#A8A8A8',
-    light:'#D2A5FF',
-    dark:'#942FFA',
-    backgroundcolor:'#FAFAFA',
-    bordercolor:'#E4E4E4',
-    active : '#A6A6A6',
-    inactive : '#E7E7E7',
-    button:'#A44EFB',
-    progressbackground:'#F5F5F5',
-    web : '#F77598',
-    mob :'#39D1B7',
-    erp : '#FFB061',
-    btncolor:'#0Da100',
-    quantity:'#ebefff',
-    Golden:'#F3E9BF',
-    platnium:'#F2E1FF',
-    grid:'#BFCEF3',
-    delete:'#F5225d'
-  };
-  '#e9e9e9'
-  
-  export default Colors;
-  
+  Black: "#000000",
+  White: "#ffffff",
+  Text: "#B3B3B3",
+  Placeholder: "#5A5A5A",
+  line: "#cccccc",
+  purple: "#A115FF",
+  lightgrey: "#EEEEEE",
+  bordercolor: "#E2E2E2",
+  lable: "#A8A8A8",
+  light: "#D2A5FF",
+  dark: "#942FFA",
+  backgroundcolor: "#FAFAFA",
+  bordercolor: "#E4E4E4",
+  active: "#A6A6A6",
+  inactive: "#E7E7E7",
+  button: "#A44EFB",
+  progressbackground: "#F5F5F5",
+  web: "#F77598",
+  mob: "#39D1B7",
+  erp: "#FFB061",
+  btncolor: "#0Da100",
+  quantity: "#ebefff",
+  Golden: "#F3E9BF",
+  platnium: "#F2E1FF",
+  grid: "#BFCEF3",
+  delete: "#F5225d",
+  Red: "#e60000",
+};
+("#e9e9e9");
+
+export default Colors;
